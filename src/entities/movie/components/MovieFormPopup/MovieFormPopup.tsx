10fc0f0b71f18a6f8c1ee7fa040ca552efaee7c0
Mykhailo Lisovyi <Mykhailo_Lisovyi@epam.com>
2022-07-03 14:45:48 +0300

    feat(movies): add forms validation

--- src/entities/movie/components/MovieFormPopup/MovieFormPopup.tsx
+++ src/entities/movie/components/MovieFormPopup/MovieFormPopup.tsx
@@ -1,5 +1,14 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  Field,
+  Form,
+  Formik,
+  FormikProps,
+  FormikValues,
+} from 'formik';
+import * as Yup from 'yup';
 
 import { PopupDialog, Spinner } from 'common/ui';
 import { State } from 'core';
@@ -15,130 +24,145 @@ interface MovieFormPopupProps {
   movie?: Movie;
 }
 
-const parseFormDataToMovieData = (formData: any): MovieData => ({
-  title: formData.title as string,
+const initialValues = {
+  title: '',
+  release_date: '',
+  poster_path: '',
+  vote_average: '',
+  genres: '',
+  runtime: '',
+  overview: '',
+};
+
+const parseMovieDataToFormData = (movie: Partial<Movie> = {}) => ({
+  title: movie.title || '',
+  release_date: movie.release_date || '',
+  poster_path: movie.poster_path || '',
+  vote_average: movie.vote_average || '',
+  genres: movie.genres ? (movie.genres[0] || '') : '',
+  runtime: movie.runtime || '',
+  overview: movie.overview || '',
+});
+
+const parseFormDataToMovieData = (formData: FormikValues): MovieData => ({
+  title: formData.title,
   tagline: 'Some test tagline',
   vote_average: parseFloat(formData.vote_average),
   vote_count: 1,
-  release_date: formData.release_date as string,
-  poster_path: formData.poster_path as string,
-  overview: formData.overview as string,
+  release_date: formData.release_date,
+  poster_path: formData.poster_path,
+  overview: formData.overview,
   budget: 100_000_000,
   revenue: 120_000_000,
-  genres: [formData.genres] as string[],
+  genres: [formData.genres],
   runtime: parseInt(formData.runtime, 10),
 });
 
-// TODO: implement form validation within the task related to forms
+const MovieFormValidationSchema = Yup.object().shape({
+  title: Yup.string().required('Required'),
+  release_date: Yup.date(),
+  poster_path: Yup.string().required('Required').url('Must be a valid URL'),
+  vote_average: Yup.number().min(0, 'Can not be less than 0'),
+  genres: Yup.string().required('Required'),
+  runtime: Yup.number().required('Required').min(1, 'Can not be less than 1'),
+  overview: Yup.string().required('Required'),
+});
 
 export function MovieFormPopup({ onClose, movie }: MovieFormPopupProps) {
+  const formRef = useRef<FormikProps<FormikValues>>(null);
   const dispatch = useDispatch();
   const loading = useSelector((state: State) => state.movies.loading);
 
-  const formRef = useRef<HTMLFormElement>(null);
-
-  const setFormValues = (movieData: Movie) => {
-    const formElement = formRef.current;
-
-    if (formElement) {
-      Object.entries(movieData).forEach(([key, value]) => {
-        // @ts-ignore
-        const element = formElement.elements[key];
-
-        if (element) {
-          element.value = Array.isArray(value) ? value[0] : value;
-        }
-      });
+  const handlePopupDialogPrimaryButtonClick = () => {
+    if (formRef.current) {
+      formRef.current.handleSubmit();
     }
   };
 
-  useEffect(() => {
-    if (movie) {
-      setFormValues(movie);
+  const handlePopupDialogSecondaryButtonClick = () => {
+    if (formRef.current) {
+      formRef.current.setValues(initialValues);
     }
-  }, []);
+  };
 
-  const handleFormSubmit = useCallback(() => {
-    const formData = new FormData(formRef.current as HTMLFormElement);
-    const parsedFormData = Object.fromEntries(formData.entries()) as any;
-    const movieData = parseFormDataToMovieData(parsedFormData);
+  const handlePopupDialogClose = useCallback(() => {
+    onClose({ confirmed: false });
+  }, [onClose]);
 
-    dispatch(movie ? requestEditMovie({ ...movieData, id: movie.id }) : requestAddMovie(movieData));
+  const handleFormSubmit = useCallback((formData: FormikValues) => {
+    const movieData = parseFormDataToMovieData(formData);
+
+    dispatch(movie ? requestEditMovie({ ...movieData, id: movie.id }) : requestAddMovie({ ...movieData }));
 
     onClose({ confirmed: true });
   }, [formRef, movie, onClose]);
 
-  const handleFormReset = useCallback(() => {
-    const formElement = formRef.current;
-
-    if (formElement) {
-      Object.keys(formElement.elements).forEach((key) => {
-        // @ts-ignore
-        formElement.elements[key].value = '';
-      });
-    }
-  }, [formRef]);
-
-  const handlePopupClose = useCallback(() => {
-    onClose({ confirmed: false });
-  }, [onClose]);
-
   return (
     <PopupDialog
       title={`${movie ? 'Edit' : 'Add'} movie`}
-      onPrimaryButtonClick={handleFormSubmit}
-      onSecondaryButtonClick={handleFormReset}
-      onClose={handlePopupClose}
+      onPrimaryButtonClick={handlePopupDialogPrimaryButtonClick}
+      onSecondaryButtonClick={handlePopupDialogSecondaryButtonClick}
+      onClose={handlePopupDialogClose}
     >
-      {loading ? <Spinner /> : (
-        <form className={styles['MovieFormPopup__form']} ref={formRef}>
-          <div className={styles['form__main-info']}>
-            <div className="form-group">
-              <label className="form-group__label">Title</label>
-              <input className="form-group__control" type="text" name="title" placeholder="Moana" />
-              <div className="form-group__validation-message">Required</div>
-            </div>
+      <Formik
+        initialValues={movie ? parseMovieDataToFormData(movie) : initialValues}
+        validationSchema={MovieFormValidationSchema}
+        onSubmit={handleFormSubmit}
+        innerRef={formRef}
+      >
+        {({ errors, submitCount }) => {
+          return loading ? <Spinner /> : (
+            <Form className={styles['MovieFormPopup__form']}>
+              <div className={styles['form__main-info']}>
+                <div className={`form-group${errors.title && submitCount > 0 ? ' form-group--has-error' : ''}`}>
+                  <label className="form-group__label">Title</label>
+                  <Field className="form-group__control" name="title" placeholder="Title" />
+                  <div className="form-group__validation-message">{errors.title as string}</div>
+                </div>
 
-            <div className="form-group">
-              <label className="form-group__label">Release Date</label>
-              <input className="form-group__control" type="date" name="release_date" placeholder="Select Date" />
-              <div className="form-group__validation-message">Required</div>
-            </div>
+                <div className={`form-group${errors.release_date && submitCount > 0 ? ' form-group--has-error' : ''}`}>
+                  <label className="form-group__label">Release Date</label>
+                  <Field className="form-group__control" type="date" name="release_date" placeholder="Select date" />
+                  <div className="form-group__validation-message">{errors.release_date as string}</div>
+                </div>
 
-            <div className="form-group">
-              <label className="form-group__label">Movie URL</label>
-              <input className="form-group__control" type="text" name="poster_path" prefix="https://" />
-              <div className="form-group__validation-message">Required</div>
-            </div>
+                <div className={`form-group${errors.poster_path && submitCount > 0 ? ' form-group--has-error' : ''}`}>
+                  <label className="form-group__label">Movie URL</label>
+                  <Field className="form-group__control" name="poster_path" placeholder="URL" />
+                  <div className="form-group__validation-message">{errors.poster_path as string}</div>
+                </div>
 
-            <div className="form-group">
-              <label className="form-group__label">Rating</label>
-              <input className="form-group__control" type="number" step="0.1" name="vote_average" placeholder="7.8" />
-              <div className="form-group__validation-message">Required</div>
-            </div>
+                <div className={`form-group${errors.vote_average && submitCount > 0 ? ' form-group--has-error' : ''}`}>
+                  <label className="form-group__label">Rating</label>
+                  <Field className="form-group__control" type="number" step="0.1" name="vote_average" placeholder="Rating score" />
+                  <div className="form-group__validation-message">{errors.vote_average as string}</div>
+                </div>
 
-            <div className="form-group">
-              <label className="form-group__label">Genre</label>
-              <select className="form-group__control" name="genres" placeholder="Select Genre">
-                {MOVIES_GENRES.map((genre) => (<option value={genre} key={genre}>{genre}</option>))}
-              </select>
-              <div className="form-group__validation-message">Required</div>
-            </div>
+                <div className={`form-group${errors.genres && submitCount > 0 ? ' form-group--has-error' : ''}`}>
+                  <label className="form-group__label">Genre</label>
+                  <Field className="form-group__control" as="select" name="genres" placeholder="Select genre">
+                    <option disabled value="">Select genre</option>
+                    {MOVIES_GENRES.map((genre) => (<option value={genre} key={genre}>{genre}</option>))}
+                  </Field>
+                  <div className="form-group__validation-message">{errors.genres as string}</div>
+                </div>
 
-            <div className="form-group">
-              <label className="form-group__label">Runtime</label>
-              <input className="form-group__control" type="number" name="runtime" placeholder="minutes" />
-              <div className="form-group__validation-message">Required</div>
-            </div>
-          </div>
+                <div className={`form-group${errors.runtime && submitCount > 0 ? ' form-group--has-error' : ''}`}>
+                  <label className="form-group__label">Runtime</label>
+                  <Field className="form-group__control" type="number" name="runtime" placeholder="Runtime (minutes)" />
+                  <div className="form-group__validation-message">{errors.runtime as string}</div>
+                </div>
+              </div>
 
-          <div className="form-group">
-            <label className="form-group__label">Overview</label>
-            <textarea className="form-group__control" rows={3} name="overview" placeholder="Movie description" />
-            <div className="form-group__validation-message">Required</div>
-          </div>
-        </form>
-      )}
+              <div className={`form-group${errors.overview && submitCount > 0 ? ' form-group--has-error' : ''}`}>
+                <label className="form-group__label">Overview</label>
+                <Field className="form-group__control" as="textarea" rows={3} name="overview" placeholder="Movie description" />
+                <div className="form-group__validation-message">{errors.overview as string}</div>
+              </div>
+            </Form>
+          );
+        }}
+      </Formik>
     </PopupDialog>
   );
 }
